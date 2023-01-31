062db2fa0eaf01397b091506c4fa217995683d2b
OakeseskAo <oakes.a@live.com>
2023-01-30 23:15:30 -0500

    Add project files.

---
+++ Models/TDUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Twoishday.Models
+{
+    public class TDUser : IdentityUser
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+
+    }
+}

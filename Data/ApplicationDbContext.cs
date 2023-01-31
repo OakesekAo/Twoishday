ce896a53c51635909d5fa29b0463e1be28922b24
OakeseskAo <oakes.a@live.com>
2023-01-30 23:27:42 -0500

    Update ApplicationDbContext.cs
    Added TDUser to be extended to DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Twoishday.Models;
 
 namespace Twoishday.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<TDUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)

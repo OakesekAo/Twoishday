b8974fc1ee2a4a5a874d7cfc20f8b4ac437affec
OakeseskAo <oakes.a@live.com>
2023-01-31 23:02:29 -0500

    First 3 ticket models
    Init models

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Twoishday.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+        [DisplayName("Status Name")]
+        public string Name { get; set; }
+    }
+}

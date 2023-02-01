91d11081863e3d2e48b2a7a44eede28f08af0fdb
OakeseskAo <oakes.a@live.com>
2023-01-31 23:31:01 -0500

    Create TicketAttachment.cs
    initialized ticket attachment model

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace Twoishday.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        public byte[] FileDate { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+
+        //navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual TDUser User { get; set; }
+    }
+}

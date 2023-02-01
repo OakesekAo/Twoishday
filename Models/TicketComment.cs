8fa207cba641bcd9d00ffb16e4a9d55c64b83cef
OakeseskAo <oakes.a@live.com>
2023-01-31 23:43:55 -0500

    Create TicketComment.cs
    Created ticketcomment model

---
+++ Models/TicketComment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+
+namespace Twoishday.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+        [DisplayName("Team Member")]
+        public string UserId { get; set;}
+
+
+
+
+        //navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual TDUser User { get; set; }
+    }
+}

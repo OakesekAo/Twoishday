c549f876ffef39f7ba950e98c6a5970d27c0052c
OakeseskAo <oakes.a@live.com>
2023-02-01 22:15:13 -0500

    Create Company.cs
    Initialized Company model class

---
+++ Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Twoishday.Models
+{
+    public class Company
+    {
+        public int Id { get; set; } //primary key
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+        //Navigation properties
+        public virtual ICollection<TDUser> Members { get; set; } = new HashSet<TDUser>();
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+    }
+}

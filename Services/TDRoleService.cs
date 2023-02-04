f562d2f6bb8ccb13eb5cc7cc956db9498ef008c7
OakeseskAo <oakes.a@live.com>
2023-02-03 23:58:45 -0500

    Create TDRoleService.cs
    Init

---
+++ Services/TDRoleService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twoishday.Models;
+using Twoishday.Services.Interfaces;
+
+namespace Twoishday.Services
+{
+    public class TDRoleService : ITDRolesService
+    {
+        public Task<bool> AddUserToRoleAsync(TDUser user, string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<TDUser>> GetUserInRoleAsync(string roleName, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<TDUser>> GetUserNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(TDUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(TDUser user, string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(TDUser user, string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(TDUser user, IEnumerable<string> roles)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

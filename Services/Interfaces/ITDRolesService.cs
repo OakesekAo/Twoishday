5b23c2728f9779525fcc1d16e474960723313179
OakeseskAo <oakes.a@live.com>
2023-02-03 23:53:27 -0500

    Update ITDRolesService.cs
    finished roleService interface

--- Services/Interfaces/ITDRolesService.cs
+++ Services/Interfaces/ITDRolesService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Twoishday.Models;
 
 namespace Twoishday.Services.Interfaces
@@ -6,5 +8,22 @@ namespace Twoishday.Services.Interfaces
     public interface ITDRolesService
     {
         public Task<bool> IsUserInRoleAsync(TDUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(TDUser user);
+
+        public Task<bool> AddUserToRoleAsync(TDUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(TDUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(TDUser user, IEnumerable<string> roles);
+
+        
+
+        public Task<List<TDUser>> GetUserInRoleAsync(string roleName, int companyId);
+
+        public Task<List<TDUser>> GetUserNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
     }
 }

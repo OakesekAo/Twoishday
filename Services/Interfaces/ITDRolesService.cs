e7dfc349e5e03cb6a13158fed8ccafcc31d2e5f7
OakeseskAo <oakes.a@live.com>
2023-02-03 23:43:49 -0500

    Update ITDRolesService.cs
    Init roles service interface

--- Services/Interfaces/ITDRolesService.cs
+++ Services/Interfaces/ITDRolesService.cs
@@ -1,6 +1,10 @@
-﻿namespace Twoishday.Services.Interfaces
+﻿using System.Threading.Tasks;
+using Twoishday.Models;
+
+namespace Twoishday.Services.Interfaces
 {
     public interface ITDRolesService
     {
+        public Task<bool> IsUserInRoleAsync(TDUser user, string roleName);
     }
 }

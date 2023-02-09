5e84b2137fe775747d77a713070cb299c13c74e3
OakeseskAo <oakes.a@live.com>
2023-02-08 21:43:34 -0500

    new interface and service
    Scaffolded out ticket service

---
+++ Services/TDTicketService.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twoishday.Models;
+using Twoishday.Services.Interfaces;
+
+namespace Twoishday.Services
+{
+    public class TDTicketService : ITDTicketService
+    {
+        public Task AddNewTicketAsync(Ticket ticket)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task ArchiveTicketAsync(Ticket ticket)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AssignTicketAsync(int ticketId, string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByCompanyAsync(int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByPriorityAsync(int companyId, string priorityName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByStatusAsync(int companyId, string statusName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByTypeAsync(int companyId, string typeName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetArchivedTicketsAsync(int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByPriorityAsync(string priorityName, int companyId, int projectId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByRoleAsync(string role, string userId, int projectId, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByTypeAsync(string typeName, int companyId, int projectId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Ticket> GetTicketByIdAsync(int ticketId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TDUser> GetTicketDeveloperAsync(int ticketId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByUserIdAsync(string userId, int companyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketPriorityIdAsync(string priorityName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketStatusIdAsync(string statusName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketTypeIdAsync(string typeName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateTicketAsync(Ticket ticket)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

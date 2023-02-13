d4dbdd9aa711d629642b30c150c61151475a64be
OakeseskAo <oakes.a@live.com>
2023-02-12 22:37:11 -0500

    Created Notification interface and service
    Created new interface and service

---
+++ Services/Interfaces/ITDNotificationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twoishday.Models;
+
+namespace Twoishday.Services.Interfaces
+{
+    public interface ITDNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification);
+
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
+
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId);
+
+        public Task SendEmailNotificationByRoleAsync(Notification notification, int companyId, string role);
+
+        public Task SendMembersEmailNotificatoinsAsync(Notification notification, List<TDUser> members);
+
+        public Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject);
+    }
+}
---
+++ Services/TDNotification.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twoishday.Data;
+using Twoishday.Models;
+using Twoishday.Services.Interfaces;
+
+namespace Twoishday.Services
+{
+    public class TDNotification : ITDNotificationService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly ITDRolesService _rolesService;
+
+        public TDNotification(ApplicationDbContext context,
+                                IEmailSender emailSender,
+                                ITDRolesService rolesService)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _rolesService = rolesService;
+        }
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            try
+            {
+                await _context.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            try
+            {
+                List<Notification> notifications = await _context.Notifications
+                                                                 .Include(n => n.Recipient)
+                                                                 .Include(n => n.Sender)
+                                                                 .Include(n => n.Ticket)
+                                                                    .ThenInclude(t => t.Project)
+                                                                 .Where(n => n.RecipientId == userId).ToListAsync();
+                return notifications;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            try
+            {
+                List<Notification> notifications = await _context.Notifications
+                                                                 .Include(n => n.Recipient)
+                                                                 .Include(n => n.Sender)
+                                                                 .Include(n => n.Ticket)
+                                                                    .ThenInclude(t => t.Project)
+                                                                 .Where(n => n.SenderId == userId).ToListAsync();
+                return notifications;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            TDUser tdUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == notification.RecipientId);
+            if (tdUser == null)
+            {
+                string tdUserEmail = tdUser.Email;
+                string message = notification.Message;
+
+                //send email
+                try
+                {
+                    await _emailSender.SendEmailAsync(tdUserEmail, emailSubject, message);
+                    return true;
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task SendEmailNotificationByRoleAsync(Notification notification, int companyId, string role)
+        {
+            try
+            {
+                List<TDUser> members = await _rolesService.GetUserInRoleAsync(role, companyId);
+                foreach (TDUser tdUser in members)
+                {
+                    notification.RecipientId = tdUser.Id;
+                    await SendEmailNotificationAsync(notification, notification.Title);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task SendMembersEmailNotificatoinsAsync(Notification notification, List<TDUser> members)
+        {
+            try
+            {
+                foreach (TDUser tdUser in members)
+                {
+                    notification.RecipientId = tdUser.Id;
+                    await SendEmailNotificationAsync(notification, notification.Title);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

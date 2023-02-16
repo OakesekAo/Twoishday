9ef088761420914556c37cd419a2244e935ac79f
OakeseskAo <oakes.a@live.com>
2023-02-15 19:46:58 -0500

    updated
    Created new controller view and model

--- Models/ViewModels/ManageUserRolesViewModel.cs
+++ Models/ViewModels/ManageUserRolesViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Twoishday.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Twoishday.Models.ViewModels
 {
     public class ManageUserRolesViewModel
     {
+        public TDUser TDUser { get; set; }
+        public MultiSelectList Roles { get; set; }
+        public List<string> SelectedRoles { get; set; }
     }
 }

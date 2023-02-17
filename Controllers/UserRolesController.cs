7a811a4aebc300536b64d0bab5c95ad7342eb32b
OakeseskAo <oakes.a@live.com>
2023-02-16 20:26:43 -0500

    udpated
    updated

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Twoishday.Services.Interfaces;
 
 namespace Twoishday.Controllers
 {
     public class UserRolesController : Controller
     {
-        public IActionResult Index()
+        private readonly ITDRolesService _roleService;
+        private readonly ITDCompanyInfoService _companyInfoService;
+        
+
+
+        public UserRolesController(ITDRolesService roleService, ITDCompanyInfoService companyInfoService)
         {
+            _roleService = roleService;
+            _companyInfoService = companyInfoService;
+        }
+        public async Task<IActionResult> ManageUserRoles()
+        {
+
             return View();
         }
     }

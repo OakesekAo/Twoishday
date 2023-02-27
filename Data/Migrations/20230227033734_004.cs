cd830c459ff23e69325918a0d134b7d7917f9409
OakeseskAo <oakes.a@live.com>
2023-02-26 22:38:00 -0500

    added migration
    Added new model feilds migration

---
+++ Data/Migrations/20230227033734_004.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Twoishday.Data.Migrations
+{
+    public partial class _004 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ArchivedByProject",
+                table: "Tickets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchivedByProject",
+                table: "Tickets");
+        }
+    }
+}

c3c5d7b7365e30196b9e179bcb44008e45a4f080
OakeseskAo <oakes.a@live.com>
2023-03-05 17:13:25 -0500

    My tickets list view
    My tickets list view

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -38,6 +38,16 @@ namespace Twoishday.Controllers
             var applicationDbContext = _context.Tickets.Include(t => t.DeveloperUser).Include(t => t.Project).Include(t => t.TicketPriority).Include(t => t.TicketStatus).Include(t => t.TicketType);
             return View(await applicationDbContext.ToListAsync());
         }
+        
+        // GET : user tickets
+        public async Task<IActionResult> MyTickets()
+        {
+            TDUser tdUser = await _userManager.GetUserAsync(User);
+
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(tdUser.Id, tdUser.CompanyId);
+
+            return View(tickets);
+        }
 
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)

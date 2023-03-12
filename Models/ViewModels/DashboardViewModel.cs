be0fcdb642228ecc0444797116e0a71e177ca9a0
OakeseskAo <oakes.a@live.com>
2023-03-11 21:01:54 -0500

    Dashboard started
    Dashboard started

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Twoishday.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<TDUser> Members { get; set; }
+    }
+}
